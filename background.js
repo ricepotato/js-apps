87f2b4a040b57b0a0226d1f20452e7afb04bf086
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-06 20:14:56 +0900

    random backgound

---
+++ background.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+
+function getRandom(){
+    return Math.floor(Math.random() * 3);
+}
+
+function init(){
+    const randomNumber = genRandom();
+
+}
+
+init();
