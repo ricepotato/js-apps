7a9141d6b568d5e33ad3a9f77669ac1e7bec4f85
ricepotato <sukjun40@naver.com>
2019-11-11 00:49:00 +0900

    js canvas event

---
+++ paintjs/app.js
@@ -0,0 +1,32 @@
+const canvas = document.querySelector("#jsCanvas");
+
+let painting = false;
+
+function stopPainting(){
+    painting = false;
+}
+
+function onMouseMove(event){
+    //console.log(event);
+    const x = event.offsetX;
+    const y = event.offsetY;
+    //console.log(x, y);
+
+}
+
+function onMouseDown(event){
+    console.log(event);
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
+}
