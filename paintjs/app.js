4e3296a67ec73c7a9d9c7615500a218ec3b43623
Sukjun Sagong <ricepotato40@gmail.com>
2019-11-11 18:13:43 +0900

    draw stroke

--- paintjs/app.js
+++ paintjs/app.js
@@ -1,4 +1,11 @@
 const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,12 +13,20 @@ function stopPainting(){
     painting = false;
 }
 
+function startPainting(){
+    painting = true;
+}
+
 function onMouseMove(event){
-    //console.log(event);
     const x = event.offsetX;
     const y = event.offsetY;
-    //console.log(x, y);
-
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event){
@@ -23,10 +38,9 @@ function onMouseUp(event){
     stopPainting();
 }
 
-
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
